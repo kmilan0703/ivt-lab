3684138bd240ba46ed009428fd8107ce50b9554e
kmilan0703 <47720451+kmilan0703@users.noreply.github.com>
2020-05-10 15:15:10 +0200

    random kivíve

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
-
+    Random generator = new SecureRandom();
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -37,7 +37,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new SecureRandom();
+
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
